19c8c7bee36076c72d50660dad1bfe13deead1f7
ricardo.bergamo <ricardo.bergamo@sesa.pr.gov.br>
2024-09-19 15:58:39 -0300

    Teste

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import os
 from traducao_individual import process_card
+from traducao_set import func_traducao
 
 app = Flask(__name__)
 
@@ -14,6 +16,17 @@ def home():
             return "Erro ao processar a carta."
     return render_template('index.html')
 
+@app.route('/traducao_set', methods=['GET', 'POST'])
+def traducao_set():
+    if request.method == 'POST':
+        set_code = request.form['set_code']
+        html_content = func_traducao(set_code)
+        return render_template('resultado_set.html', html_content=html_content)
+    return render_template('index_set.html')
+
+@app.route('/resultado_set/<set_code>')
+def resultado_set(set_code):
+    return redirect(url_for('traducao_set'))
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ traducao_set.py
@@ -0,0 +1,149 @@
+import pandas as pd
+from deep_translator import GoogleTranslator
+import requests
+import csv
+from tqdm import tqdm
+import webbrowser
+import os
+
+
+def download_json(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    elif response.status_code == 404:
+        raise Exception("JSON download failed due to 404 error")
+    else:
+        raise Exception(f"JSON download failed with status code: {response.status_code}")
+
+
+def process_card_data(card):
+    try:
+        oracle_texto = card["oracle_text"]
+    except KeyError:
+        try:
+            oracle_texto = card['card_faces'][0]['oracle_text'] + '\n' + '----' + '\n' + card['card_faces'][1][
+                'oracle_text']
+        except KeyError:
+            oracle_texto = ''
+    return {
+        "num": card.get("collector_number", ""),
+        "name": card["name"],
+        "oracle_text": oracle_texto
+    }
+
+
+def filter_card_data(df):
+    df = df[~df['num'].str.contains('z')]
+    names_to_drop = ['Plains', 'Swamp', 'Island', 'Mountain', 'Forest']
+    return df[~df['name'].isin(names_to_drop)]
+
+
+def translate_and_format_text(text):
+    text = GoogleTranslator(source='auto', target='pt').translate(text=text)
+    return text.replace('\n', '<br>')
+
+
+def translate_card_texts(texts, set_code):
+    translated_texts = []
+    max_char_limit = 5000
+    for text in tqdm(texts, desc=f"Translating {set_code}"):
+        # Aplica a tradução e formatação individual primeiro
+        formatted_text = translate_and_format_text(text)
+
+        if isinstance(formatted_text, str) and len(formatted_text) > max_char_limit:
+            chunks = [formatted_text[i:i + max_char_limit] for i in range(0, len(formatted_text), max_char_limit)]
+            translated_texts_chunk = []
+            for chunk in chunks:
+                translated = GoogleTranslator(source='auto', target='pt').translate(text=chunk)
+                translated_texts_chunk.append(translated)
+            translated_texts.append(' '.join(translated_texts_chunk))
+        else:
+            translated = GoogleTranslator(source='auto', target='pt').translate(text=formatted_text)
+            translated_texts.append(translated)
+    return translated_texts
+
+
+def save_csv_file(df, file_path):
+    df.to_csv(file_path, index=False, encoding='utf-8')
+
+
+def create_html_table(rows):
+    html_table = '<table class="styled-table">\n'
+    for row in rows:
+        html_table += '  <tr>\n'
+        for cell in row:
+            # Replace '\n' with '<br>' in cell content
+            cell_content = cell.replace('\n', '<br>')
+            html_table += f'    <td>{cell_content}</td>\n'
+        html_table += '  </tr>\n'
+    html_table += '</table>'
+    return html_table
+
+
+def read_html_template(template_path):
+    with open(template_path, "r", encoding="utf-8") as file:
+        return file.read()
+
+
+def fill_html_template(template_content, set_code, html_table):
+    return template_content.format(set_code=set_code, html_table=html_table)
+
+
+def save_html_file(html_content, file_path):
+    with open(file_path, "w", encoding="utf-8") as html_file:
+        html_file.write(html_content)
+
+
+def open_html_file(html_file_path):
+    webbrowser.open(html_file_path)
+
+
+def func_traducao(set_code):
+    try:
+        set_url = f"https://api.scryfall.com/sets/{set_code}"
+        set_json = download_json(set_url)
+        if set_json and 'search_uri' in set_json:
+            url = set_json['search_uri']
+        else:
+            raise Exception("Falha ao obter o URL de pesquisa do conjunto JSON")
+
+        all_card_data = []
+        while url:
+            json_data = download_json(url)
+            all_card_data.extend([process_card_data(card) for card in json_data["data"]])
+            url = json_data.get("next_page")
+
+        df = pd.DataFrame(all_card_data)
+
+        if not df.empty:
+            df.index += 1
+            df['oracle_text'] = df['oracle_text'].fillna(value='DEBUG', inplace=False)
+
+        df = filter_card_data(df)
+
+        translated_texts = translate_card_texts(df['oracle_text'].tolist(), set_code)
+
+        df['traduzido'] = translated_texts
+
+        df = df.rename(columns={'num': 'numero', 'name': 'nome', 'oracle_text': 'texto_ingles'})
+
+        save_csv_file(df, 'traducao.csv')
+
+        with open('traducao.csv', 'r', encoding='utf-8') as csvfile:
+            csvreader = csv.reader(csvfile)
+            rows = list(csvreader)
+
+        html_table = create_html_table(rows)
+
+        # Você pode retornar o HTML diretamente aqui
+        return html_table
+
+    except Exception as e:
+        print(e)
+        return f"Erro: {e}"
+
+
+
+if __name__ == "__main__":
+    func_traducao()
